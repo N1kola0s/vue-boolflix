10e0100b49b5a21c009210a9a4114c83c75dd3e0
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-09 13:04:08 +0200

    add call api add search box

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div class="container mt-5">
+      <form class="d-flex justify-content-center align-items-center" action="">
+        <input type="text" v-model="searchText" >
+        <button @click.prevent="searchMovie" class="mx-3 p-1">Cerca</button>
+      </form>
+
+    </div>
+    <!-- /.container -->
+   
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//importo axios nel tag script 
+import axios from "axios"; 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data(){
+    return {
+      url:'https://api.themoviedb.org/3/search/movie?api_key=a10bb2f450a66787dd09fbc8afd56539&language=it-IT&page=1&include_adult=false&query=matrix',
+      movies: null,
+      loading: true,
+      error: null,
+      searchText: ''
+
+    };
+  },
+  methods:{
+    searchMovie(){
+      /* console.log('Searching...'); */
+      console.log(this.searchText);
+    },
+    callApi(){
+      axios.get(this.url).then(response => {
+        console.log(this);
+        console.log(response);
+
+        this.movies = response.data.response
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  },
+  mounted(){
+            this.callApi(); 
+        },
 }
 </script>
 
 <style lang="scss">
 /* importo lo stile */
   @import '@/assets/scss/style.scss';
+  
+  
+  form{
+    background-color: aquamarine;
+    height: 100px;
+
+  }
 </style>
