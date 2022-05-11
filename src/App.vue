c20ade5aaa9559677e61c2bc52448393063a1212
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-11 22:02:19 +0200

    add stylesheet add codnition on overview add scrollbar in cards

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
         <div class="row row-cols-6 g-3">
           <div class="col gap-3" v-for="movie in movies" :key="movie.id">
 
-            <div class="card">
+            <div class="card border-0">
               
                 <img class="card-img-top" v-if="movie.poster_path!= null" :src=" 'https://image.tmdb.org/t/p/w342/' + movie.poster_path" alt="poster film">
                 <img class="card-img-top" src="https://picsum.photos/id/209/342/500" v-else>
@@ -32,16 +32,23 @@
               <div class="card-body w-100">
                 <h5 class="card-title">{{movie.title}}</h5>
                 <h6 class="card-subtitle mb-2 text-muted original_title">{{movie.original_title}}</h6>
-                <p class="card-text">
-                  Lingua: <!-- {{movie.original_language}} --> <flag :iso = "countryFilter(movie.original_language)"></flag> <br>
-                  Voto: {{Math.round((movie.vote_average) / 2)}}/5
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 1" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 2" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 3" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 4" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 5" />
-                </p>
-                <!-- /.card-text -->
+                <div class="card-text">
+                  <div class="info my-3">
+                    Lingua: <!-- {{movie.original_language}} --> <flag :iso = "countryFilter(movie.original_language)"></flag> <br>
+                    Voto: {{Math.round((movie.vote_average) / 2)}}/5
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 1" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 2" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 3" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 4" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((movie.vote_average) / 2)>= 5" />
+                  </div>
+                  <!-- /.info -->
+                  <div class="card-text overview" v-show="movie.overview != ''" >
+                    <h6>Descrizione:</h6>{{movie.overview}}
+                  </div>
+                  <!-- /.overview --> 
+                </div>
+                <!-- ./card-text -->
               </div>
               <!-- /.card-body -->
             </div>
@@ -50,7 +57,7 @@
           <!-- /.col -->
 
           <div class="col" v-for="serie in series" :key="serie.id">
-            <div class="card">
+            <div class="card border-0">
               
                 <img class="card-img-top" v-if="serie.poster_path!= null" :src=" 'https://image.tmdb.org/t/p/w342/' + serie.poster_path" alt="poster film">
                 <img class="card-img-top" src="https://picsum.photos/id/209/342/500" v-else>
@@ -60,16 +67,24 @@
               <div class="card-body w-100">
                 <h5 class="card-title">{{serie.name}}</h5>
                 <h6 class="card-subtitle mb-2 text-muted original_title">{{serie.original_name}}</h6>
-                <p class="card-text">
-                  Lingua: <!-- {{serie.original_language}} --> <flag :iso = "countryFilter(serie.original_language)"></flag> <br>
-                  Voto: {{Math.round((serie.vote_average) / 2)}}/5
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 1" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 2" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 3" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 4" />
-                  <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 5" />
-                </p>
+                <div class="card-text">
+                  <div class="info my-3">
+                    Lingua: <!-- {{serie.original_language}} --> <flag :iso = "countryFilter(serie.original_language)"></flag> <br>
+                    Voto: {{Math.round((serie.vote_average) / 2)}}/5
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 1" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 2" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 3" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 4" />
+                    <font-awesome-icon class="vote_star" :icon="['fas', 'star']" v-show="Math.round((serie.vote_average) / 2)>= 5" />
+                  </div>
+                  <!-- /.info -->
+                  <div class="overview" v-show="serie.overview!=''">
+                    <h6>Descrizione:</h6>{{serie.overview}}
+                  </div>
+                  <!-- /.overview -->
+                </div>
                 <!-- /.card-text -->
+               
               </div>
               <!-- /.card-body -->
             </div>
@@ -109,10 +124,7 @@ export default {
     };
   },
   methods:{
-    /* searchMovie(){
-      console.log('Searching...'); 
-      console.log(this.searchText);
-    }, */
+
     callApi(){
       axios
         // richiesta
@@ -166,6 +178,9 @@ export default {
   header{
     height:70px;
     background-color: #1B1B1B;
+    /* background-color: #000000; */
+    position: sticky;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 50%);
     
     nav{
       .logo{
@@ -191,7 +206,7 @@ export default {
   }
 
   main{
-    min-height: calc(100vh - 70px);
+    max-height: calc(100vh - 70px);
 
     .container{
       .row{
@@ -199,15 +214,37 @@ export default {
           .card{
             .card-img-top{
               /* width: 342px; */
-              height: 298px;
+              max-height: 298px;
+              min-height: 298px;
+              
+              
             }
 
             .card-body{
               /* width: 342px; */
               display:none;
-              height: 298px;
+              max-height: 298px;
+              min-height: 298px;
               background-color:black;
               color:white;
+              overflow: auto;
+                /* Works on Firefox */
+                scrollbar-width: thin;
+                scrollbar-color: #1B1B1B #DA181D;
+                /* Works on Chrome, Edge, and Safari */
+                &::-webkit-scrollbar {
+                    width: 8px;
+                }
+                &::-webkit-scrollbar-track {
+                    background: black;
+                }
+                &::-webkit-scrollbar-thumb {
+                    background-color: #DA181D;
+                    border-radius: 16px;
+                    border: 2px solid black;
+                }
+                /* background-color: #1B1B1B; */
+
                 .vote_star{
                 color: yellow;
                 }
@@ -229,8 +266,4 @@ export default {
 
     
   }
-  
-  
-  
-  
 </style>
